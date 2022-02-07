72816516066fe9038653fa36c837a178b5e5872f
SSkoufalos <greencomet88@yahoo.com>
2022-02-06 21:06:43 -0500

    Upgrade 4.1

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,23 +1,21 @@
-import { 
-Component, 
-OnInit 
-} from '@angular/core';
-
-declare var require: any;
-
-var data = require('../../assets/example.JSON');
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pokemon',
+  selector: 'my-app',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.scss']
+  styleUrls: ['./pokemon.component.css'],
 })
-export class PokemonComponent implements OnInit {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  OurPokemon: any = {};
 
-  constructor() { }
+  pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/porygon';
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get(this.pokemonUrl).subscribe((data) => {
+      this.OurPokemon = data;
+    });
   }
-
 }
